00e13df1815ebd1c8a0f8a160ec354a5115745a3
migueltovar <loganitsolutions@outlook.com>
2024-11-20 23:37:54 -0500

    if context then do nothing

--- AbcallApp/contexts/AuthContext.tsx
+++ AbcallApp/contexts/AuthContext.tsx
@@ -24,7 +24,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
+    //throw new Error('useAuth must be used within an AuthProvider');
   }
   return context;
 };
