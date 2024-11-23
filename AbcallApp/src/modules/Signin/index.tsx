ad04cff3a433fdc8913405e958a9b179e086ec16
migueltovar <loganitsolutions@outlook.com>
2024-11-23 15:32:43 -0500

    removing commented code

--- AbcallApp/src/modules/Signin/index.tsx
+++ AbcallApp/src/modules/Signin/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { HeaderLanguage } from '@components/Signin/HeaderLanguage';
 import { useAuth } from '@contexts/AuthContext';
-import {Button, View, Text } from 'react-native';
+import {View} from 'react-native';
 import { Logo } from '@components/Signin/Logo';
 import { Tabs } from '@components/Signin/Tabs';
 import { FormLogin } from '@components/Signin/Form';
@@ -21,8 +21,6 @@ const Signin = () => {
         <Logo/>
         <Tabs/>
         <FormLogin/>
-        <Text>{isLoggedIn ? '¡Ya estás autenticado!' : 'Por favor, inicia sesión'}</Text>
-        <Button title="Iniciar Sesión" onPress={() => setLoggedIn(true)} />
       </View>
     );
 };
